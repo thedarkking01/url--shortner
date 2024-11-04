4d7cee493c7666b4b46387f898faf997f5c51ba9
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-11-04 19:41:34 +0530

    URL API set

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import BackgroundAnimate from './BackgroundAnimate';
 import InputShortener from './InputShortener';
-// import LinkResult from './LinkResult';
+import LinkResult from './LinkResult';
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -11,7 +11,7 @@ function App() {
     <div className="container">
       <InputShortener setInputValue={setInputValue} />
       <BackgroundAnimate />
-      {/* <LinkResult inputValue={inputValue} /> */}
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
